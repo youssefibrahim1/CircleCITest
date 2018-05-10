1c2e04a2e99fb087e4108dd237beb773b05d9121
sulaimandev <youssef.ibrahim@mail.utoronto.ca>
2018-05-10 15:43:34 -0400

    removed required files

--- scripts/displayMessage.js
+++ scripts/displayMessage.js
@@ -1,13 +1,13 @@
-let readline = require('readline-sync');
-let axios = require('axios');
-let fs = require('fs');
-let config = require('../config.js');
-let path = require('path');
-// let { APIProdkey } = config.APIKeys;
+// let readline = require('readline-sync');
+// let axios = require('axios');
+// let fs = require('fs');
+// let config = require('../config.js');
+// let path = require('path');
+// // let { APIProdkey } = config.APIKeys;
 
 
-const { readdirSync, lstatSync } = require('fs')
-const { join } = require('path')
+// const { readdirSync, lstatSync } = require('fs')
+// const { join } = require('path')
 
 function sum(a,b){
     'use strict';
