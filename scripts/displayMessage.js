9265a688537d9fd65dbb690f70110d2473fe4f35
sulaimandev <youssef.ibrahim@mail.utoronto.ca>
2018-05-10 15:34:54 -0400

    fixed use strict location

--- scripts/displayMessage.js
+++ scripts/displayMessage.js
@@ -5,11 +5,12 @@ let config = require('../config.js');
 let path = require('path');
 // let { APIProdkey } = config.APIKeys;
 
-'use strict';
+
 const { readdirSync, lstatSync } = require('fs')
 const { join } = require('path')
 
 function sum(a,b){
+    'use strict';
     return a+b;
  }
  module.exports=sum
--- scripts/msg.test.js
+++ scripts/msg.test.js
@@ -1,8 +1,8 @@
 
-'use strict';
 const sum =require('./displayMessage')
 
 test('Adds two numbers', sumTest)
 function sumTest(){
+    'use strict';
     expect(sum(1,2)).toBe(3);
 }
